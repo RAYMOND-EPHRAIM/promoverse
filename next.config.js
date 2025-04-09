50fd62a8e0faaa8d92cbf6fcdb1d0a484d062ab3
RAYMOND-EPHRAIM <EPHARIMRAY@GMAIL.COM>
2025-04-09 11:57:59 -0400

    Run dev fix

--- next.config.js
+++ next.config.js
@@ -2,11 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    serverActions: {},
+    serverActions: true,
+    serverComponentsExternalPackages: ['@prisma/client'],
   },
   eslint: {
     ignoreDuringBuilds: true, // ✅ THIS is the key
   },
+  images: {
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+  },
+  allowedDevOrigins: ['localhost:3000', '100.115.92.198:3000'],
 };
 
 module.exports = nextConfig;
