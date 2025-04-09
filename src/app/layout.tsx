be551d007c3627c5ba0d2898a8db34ffb07aee67
RAYMOND-EPHRAIM <EPHARIMRAY@GMAIL.COM>
2025-04-09 11:50:17 -0400

    Port fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,11 +11,14 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'PromoVerse',
   description: 'The Universe Where Anything Can Be Promoted',
-  themeColor: '#4f46e5',
   icons: [
     { rel: 'icon', url: '/icon-192.png' }
   ],
   manifest: '/manifest.json'
+};
+
+export const viewport = {
+  themeColor: '#4f46e5'
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
